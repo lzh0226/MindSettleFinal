d5417e5978b39aa9f99c125fb77795ed3c374b6b
lzh0226 <lzh521201>
2020-01-10 04:01:39 +0800

    2nd Commit

---
+++ app/src/main/java/com/example/mindsettle/ResponseGratitude.kt
@@ -0,0 +1,11 @@
+package com.example.mindsettle
+
+//import javax.annotation.Generated
+import com.google.gson.annotations.SerializedName
+
+//@Generated("com.robohorse.robopojogenerator")
+data class ResponseGratitude(
+
+	@field:SerializedName("records")
+	val records: List<Gratitude?>? = null
+)
